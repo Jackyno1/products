910245c0289b22659dc5b9918291c71679224b88
Jack Chang <a5170159@gmail.com>
2022-05-02 16:33:21 +0800

    add write csv file

--- products.py
+++ products.py
@@ -11,3 +11,8 @@ print(products)
 for p in products:
 	print(p[0], '的價格為', p[1])
 
+#加入write功能
+with open('products.csv', 'w') as f: #用逗點可讓content在不同的CSV格子裡
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
