193fa88e954c040c13bd62d718f9fdea91a802f6
Jack Chang <a5170159@gmail.com>
2022-05-17 19:37:10 +0800

    add function

--- products.py
+++ products.py
@@ -1,37 +1,46 @@
 # 找檔案在不在
 import os
-if os.path.isfile('products.csv'):
-	print('yeah! 找到檔案了')
 
 # 讀取檔案
+def read_file(filename):
 	products = []
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if '商品,價格' in line:
-				continue
-			name, price = line.strip().split(',')
-			products.append([name,price])
-	print(products)
-
-else: 
-	print('找不到檔案...')
+	if os.path.isfile(filename):
+		print('yeah! 找到檔案了')
+		with open(filename, 'r', encoding = 'utf-8') as f:
+			for line in f:
+				if '商品,價格' in line:
+					continue
+				name, price = line.strip().split(',')
+				products.append([name,price])
+		print(products)
+	else: 
+		print('找不到檔案....')
+	return products
 
 # 讓使用者輸入
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q': #quit
-	    break
-    price = input('請輸入商品金額:')
-    products.append([name,price])
-print(products)
+def user_input(products):
+	while True:
+	    name = input('請輸入商品名稱:')
+	    if name == 'q': #quit
+		    break
+	    price = input('請輸入商品金額:')
+	    products.append([name,price])
+	print(products)
+	return products
 
 # add for loop
-for p in products:
-	print(p[0], '的價格為', p[1])
+def price_tag(products):
+	for p in products:
+		print(p[0], '的價格為', p[1])
 
 # 加入write功能
-with open('products.csv', 'w', encoding = 'utf-8') as f: #encoding用了utf-8才不會亂碼
-	f.write('商品,價格\n') #用逗號區隔!!
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n') #用逗點可讓content在不同的CSV格子裡 
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f: #encoding用了utf-8才不會亂碼
+		f.write('商品,價格\n') #用逗號區隔!!
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n') #用逗點可讓content在不同的CSV格子裡 
 
+products = read_file('products.csv')
+products = user_input(products)
+price_tag(products)
+write_file('products.csv', products)
